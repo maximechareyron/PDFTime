2789ef059d9c951050dbde4c0f9f20b467dd4e02
jeromeM <jeromem@debian>
2016-12-08 17:14:16 +0100

    Script des annimations de pages et separation des script et du HTML dans js/Main.js

--- js/Main.js
+++ js/Main.js
@@ -30,4 +30,59 @@
         return false;
     }
 
-}(jQuery);
+}(jQuery)
+
+
+
+
+//Fonction pour la page extraction
+
+
+function visibility(thingId)
+{
+        var targetElement;
+        targetElement = document.getElementById(thingId) ;
+        if (targetElement.style.display == "none" )
+        {
+            targetElement.style.display = "" ;
+            if(thingId=='my_help') {
+                document.getElementById('btnHelp').className = "btn btn-info";
+            }
+        }
+        else {
+            targetElement.style.display = "none" ;
+
+            if(thingId=='my_help') {
+                document.getElementById('btnHelp').className = "btn btn-default";
+            }
+        }
+}
+
+
+//Fonction pour la pages de fusion
+
+function addNewFile() {
+
+    var MaDiv = document.createElement('div');
+    MaDiv.className='text-left';
+    MaDiv.id=('add');
+    MaDiv.innerHTML='Selectionner un PDF à fusionner\
+                        <form>\
+                        <div class="input-group" style="margin-bottom:2.5%;">\
+                            <input type="text" class="form-control" placeholder="Chemin vers fichier">\
+                            <div class="input-group-btn">\
+                            <button class="btn btn-default" type="file">\
+                            Parcourir\
+                            </button>\
+                            </div>\
+                            </div>\
+                            </form>';
+    document.getElementById('zone_nbFile').appendChild(MaDiv);
+
+}
+
+function rmFile(){
+    var element = document.getElementById('add')// element à supprimer
+    element.parentNode.removeChild(element);
+
+}
