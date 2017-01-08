6566e59c186a11ce37d45e918a8d3acba0cd7ca5
jeromeM <jeromem@debian>
2017-01-08 17:13:54 +0100

    insertion d'un pdf

--- js/Main.js
+++ js/Main.js
@@ -118,3 +118,25 @@ function Validation_Fusion()
     listPathFile = new Array();
     alertSave(); ///realisation en cours
 }
+
+
+///PDF Display///
+
+(function(a){a.createModal=function(b){defaults={title:"",message:"PDFVIEW",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 420px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
+
+
+//PDF Gesture
+
+function nextPage(){
+    var pdf  = document.getElementById('pdf');
+    //noinspection JSAnnotator
+    pdf.getAttribute('pageNumber')= pdf.getAttribute('pageNumber')+1;
+
+}
+
+function prevPage(){
+    var pdf  = document.getElementById('pdf');
+    //noinspection JSAnnotator
+    pdf.getElementById('pageNumber') = pdf.getElementById('pageNumber')+1;
+
+}
