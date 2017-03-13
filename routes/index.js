4c718a5f7a6b114d1650080f56843995834946be
Alex <alex.donne@live.fr>
2017-03-13 10:41:16 +0100

    Noms fichiers aléatoires + petit prob résolu
    	modified:   modules/pdftk.js
    	deleted:    npm-debug.log
    	modified:   routes/index.js
    	deleted:    upload_2c164950d082c5462968e658f303030b
    	deleted:    upload_68e15fd1c2be56b56043b6b0577ce212
    	deleted:    upload_75a593cc7120aadff1093e53cfcc1776
    	deleted:    upload_83fda2cb04689a10605ce46e14b7a74f
    	deleted:    upload_bb368f3803d071552fd233dade0d8c0a

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,46 @@ router.get('/formulaire', function(req, res, next) {
 });
 
 router.post('/fusion/upload', function(req, res){
-    upload(req,res,function (fichiers){pdftk.fusion(fichiers);});
+    var fichiers=[];
+    var i=0;
+    var filename;
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
+    form.on('file', function (field, file) {
+        filename=file.path;
+        fichiers[i]=filename;
+        i++;
+    });
+
+    // log any errors that occur
+    form.on('error', function(err) {
+        console.log('An error has occured: \n' + err);
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function(err) {
+        var out = filename.split('\\').pop();
+        out=out+".pdf";
+        async.series([
+                function (callback){  pdftk.fusion(fichiers,out) ;   callback()}],
+            function(){res.sendFile(out, {root: path.join(__dirname, '/..')},function (err){
+                if (err){
+                    console.log(err);
+                }
+                else {
+                    exec('rm ' +out);
+                }
+            });});
+
+    });
+    // parse the incoming request containing the form data
+    form.parse(req);
 });
 
 router.post('/extraction/upload', function(req, res){
@@ -89,63 +128,37 @@ router.post('/uploadform2', function(req, res){
 });
 
 router.post('/uploadform2/formRempli', function(req,res){
+    //req.body affectée dans une autre variable pour pouvoir accéder aux champs facilement sans avoir la valeur du "name"
     var post=req.body;
-    console.log(req.body);
+    var cle2;
     var contenu= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+ "<xfdf xmlns=\"http://ns.adobe.com/xfdf/\" xml:space=\"preserve\">"+ " <fields>";
     for (var cle in post){
-        if (post[cle] == "on") {
+        if (post[cle] == "on" && cle.substring(cle.length-3,cle.length)=="chk") { //Si l'utilisateur veut taper "on" dans un autre champ
+            //La valeur d'une chackbox cochée est "on" dans le req.body, et il faut mettre "Yes" dans le fdf, mais il faut que l'utilisateur puisse taper "on"
+            // dans un autre champ sans que la valeur soit remplacée par "Yes"
             post[cle] = "Yes";
+            cle2=cle.substring(0,cle.length-3);
         }
-        contenu+="\<field name="+"\""+cle+"\""+"\><value>"+post[cle]+"\</value>\</field>";
+        else {
+            cle2=cle;
+        }
+        contenu+="\<field name="+"\""+cle2+"\""+"\><value>"+post[cle]+"\</value>\</field>";
     }
     contenu +="\</fields></xfdf>";
-    fs.writeFileSync(path.join(path.join(__dirname,"/.."),"formrempli.fdf"),contenu);
-    pdftk.remplirPdf();
-    res.sendFile("result.pdf", {root: path.join(__dirname, '/..')},function (err){
-        exec('rm result.pdf');
+
+    //Ecrire le fichier sur le serveur
+    var fdf= Date.now()+".fdf";
+    fs.writeFileSync(path.join(path.join(__dirname,"/.."),fdf),contenu);
+    var out= Date.now()+".pdf";
+    console.log(out);
+    pdftk.remplirPdf(out,fdf);
+
+    res.sendFile(out, {root: path.join(__dirname, '/..')},function (err){
+        exec('rm '+out);
     });
 });
 
-function upload(req,res,fonctionPdftk){
-    var fichiers=[];
-    var i=0;
-    // create an incoming form object
-    var form = new formidable.IncomingForm();
 
-    // specify that we want to allow the user to upload multiple files in a single request
-    form.multiples = true;
-
-    // store all uploads in the /uploads directory
-    form.uploadDir = path.join(__dirname, '/uploads');
-    form.on('file', function (field, file) {
-        var filename=i+".pdf";
-        fichiers[i]=filename;
-        fs.rename(file.path, path.join(form.uploadDir, filename));
-        i++;
-    });
-
-    // log any errors that occur
-    form.on('error', function(err) {
-        console.log('An error has occured: \n' + err);
-    });
-
-    // once all the files have been uploaded, send a response to the client
-    form.on('end', function(err) {
-        async.series([
-                function (callback){ fonctionPdftk(fichiers,req.body.numsPages) ;   callback()}],
-            function(){res.sendFile("result.pdf", {root: path.join(__dirname, '/..')},function (err){
-                if (err){
-                    console.log(err);
-                }
-                else {
-                    exec('rm result.pdf');
-                }
-            });});
-
-    });
-    // parse the incoming request containing the form data
-    form.parse(req);
-}
 
 
 
